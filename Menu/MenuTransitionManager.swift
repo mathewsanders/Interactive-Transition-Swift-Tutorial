b6c9103c8f42535448e6dbed16072ea6f8342259
Mathew Sanders <mathew.sanders@gmail.com>
2014-09-14 22:06:10 -0400

    Added interactive transition to present menu

--- Menu/MenuTransitionManager.swift
+++ Menu/MenuTransitionManager.swift
@@ -26,7 +26,35 @@ class MenuTransitionManager: UIPercentDrivenInteractiveTransition, UIViewControl
     
     // TODO: We need to complete this method to do something useful
     func handleOnstagePan(pan: UIPanGestureRecognizer){
-        println("Todo: handle onstage gesture...")
+        // how much distance have we panned in reference to the parent view?
+        let translation = pan.translationInView(pan.view!)
+        
+        // do some math to translate this to a percentage based value
+        let d =  translation.x / CGRectGetWidth(pan.view!.bounds) * 0.5
+        
+        // now lets deal with different states that the gesture recognizer sends
+        switch (pan.state) {
+            
+        case UIGestureRecognizerState.Began:
+            // set our interactive flag to true
+            self.interactive = true
+            
+            // trigger the start of the transition
+            self.sourceViewController.performSegueWithIdentifier("presentMenu", sender: self)
+            break
+            
+        case UIGestureRecognizerState.Changed:
+            
+            // update progress of the transition
+            self.updateInteractiveTransition(d)
+            break
+            
+        default: // .Ended, .Cancelled, .Failed ...
+            
+            // return flag to false and finish the transition
+            self.interactive = false
+            self.finishInteractiveTransition()
+        }
     }
     
     // MARK: UIViewControllerAnimatedTransitioning protocol methods
